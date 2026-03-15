288e26106581e8ebe19eb2a86df280a9bc9846ea
ivorydevrimoalt <154000627+ivorydevrimoalt@users.noreply.github.com>
2026-03-15 20:18:31 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -5,18 +5,22 @@ var mixer, morphs = [];
 var playerFactory, particle, color, id;
 var players = [];
 var bullets = [];
-var xyzLimit = 500;
+var xyzLimit = 480; 
 var clock = new THREE.Clock();
 var textureLoader = new THREE.TextureLoader();
 var bulletMap = textureLoader.load("textures/sprite.png");
 var loader = new THREE.JSONLoader();
 
+// Physics variables
+var velocity = new THREE.Vector3();
+var canJump = false;
+var isCrouching = false;
+var PLAYER_HEIGHT = 25;
+var CROUCH_HEIGHT = 12;
+
 // Socket.io
-socket.on('init', function (socketID) {
-    id = socketID;
-});
+socket.on('init', function (socketID) { id = socketID; });
 socket.on('player', function (player) {
-    console.log(player);
     if ('online' in player) {
         playerHandler(player.online[0], player.online[1][0], player.online[1][1]);
     } else if ('offline' in player) {
@@ -25,189 +29,159 @@ socket.on('player', function (player) {
     }
 });
 socket.on('bullet', function (bullet) {
-    console.log(bullet);
     var position = new THREE.Vector3(bullet[0].x, bullet[0].y, bullet[0].z);
     var speed = new THREE.Vector3(bullet[1].x, bullet[1].y, bullet[1].z);
     AddBullet(position, speed, bullet[2]);
-});
-socket.on('hit', function (data) {
-    console.log(data);
-    if (id == data.hit) {
-        var answer = confirm("You are dead, play again?");
-        if (answer) {
-            window.location.reload();
-        } else {
-            window.location = "about:blank";
-        }
-    } else {
-        if (id == data.by) {
-            console.log("You killed: " + data.hit);
-        }
-        scene.remove(players[data.hit]);
-    }
-});
-socket.on('connect', function () {
-    console.log('Connected');
-});
-socket.on('disconnect', function () {
-    console.log('Disconnected');
-});
-socket.on('reconnect', function () {
-    console.log('Reconnected to server');
-});
-socket.on('reconnecting', function (nextRetry) {
-    console.log('Attempting to re-connect to the server, next attempt in ' + nextRetry + 'ms');
-});
-socket.on('reconnect_failed', function () {
-    console.log('Reconnected to server FAILED.');
 });
 
 init();
 animate();
 
 // Fire
-$(this).click(function () {
-    var speed = camera.getWorldDirection().multiplyScalar(20); // create speed vactor
-    AddBullet(camera.position, speed);
-    socket.emit('bullet', [controls.object.position, speed]);
+$(renderer.domElement).click(function () {
+    if (document.pointerLockElement === renderer.domElement) {
+        var speed = camera.getWorldDirection().multiplyScalar(20);
+        AddBullet(camera.position, speed);
+        socket.emit('bullet', [controls.object.position, speed]);
+    }
 });
 
 function init() {
-    // Stats Monitor
     stats = new Stats();
     stats.showPanel(0);
     document.body.appendChild(stats.dom);
 
-    // Camera
     camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
-    camera.position.set(Math.random() * 600 - 300, 25, Math.random() * 600 - 300);// Random create initial position [-400, 400]
+    camera.position.set(-400, PLAYER_HEIGHT, -400); // Start in a corner
 
-    // Scene
     scene = new THREE.Scene();
-    //scene.fog = new THREE.FogExp2(0x000000, 0.0025);
 
-    // Light
-    light = new THREE.DirectionalLight(0xffffff);
-    light.position.set(0, 0.5, 1).normalize();
-    scene.add(light);
+    // --- LIGHTING ---
+    scene.add(new THREE.AmbientLight(0xffffff, 1.0)); 
 
-    // Skybox
-    var materials = [
-        new THREE.MeshBasicMaterial({map: textureLoader.load('textures/cube/skybox/px.jpg')}), // right
-        new THREE.MeshBasicMaterial({map: textureLoader.load('textures/cube/skybox/nx.jpg')}), // left
-        new THREE.MeshBasicMaterial({map: textureLoader.load('textures/cube/skybox/py.jpg')}), // top
-        new THREE.MeshBasicMaterial({map: textureLoader.load('textures/cube/skybox/ny.jpg')}), // bottom
-        new THREE.MeshBasicMaterial({map: textureLoader.load('textures/cube/skybox/pz.jpg')}), // back
-        new THREE.MeshBasicMaterial({map: textureLoader.load('textures/cube/skybox/nz.jpg')})  // front
+    // --- ROOM & MAZE CONFIG ---
+    var roomWidth = 1000;
+    var roomHeight = 400; 
+    var roomDepth = 1000;
+    var wallColor = 0x6d33a0;
+
+    // Room Shell
+    var wallMat = new THREE.MeshBasicMaterial({ color: wallColor, side: THREE.BackSide });
+    var floorTex = textureLoader.load('./textures/room/floor.jpg');
+    floorTex.wrapS = floorTex.wrapT = THREE.RepeatWrapping;
+    floorTex.repeat.set(10, 10);
+    var floorMat = new THREE.MeshBasicMaterial({ map: floorTex, side: THREE.BackSide });
+    var ceilMat = new THREE.MeshBasicMaterial({ map: textureLoader.load('./textures/room/top.png'), side: THREE.BackSide });
+
+    var roomMaterials = [wallMat, wallMat, ceilMat, floorMat, wallMat, wallMat];
+    var room = new THREE.Mesh(new THREE.BoxGeometry(roomWidth, roomHeight, roomDepth), new THREE.MultiMaterial(roomMaterials));
+    room.position.y = (roomHeight / 2) - 5; 
+    scene.add(room);
+
+    // --- MAZE GENERATION ---
+    // 1 = Wall, 0 = Path
+    var mazeData = [
+        [1,1,1,1,1,1,1,1,1,1],
+        [1,0,0,0,1,0,0,0,0,1],
+        [1,0,1,0,1,0,1,1,0,1],
+        [1,0,1,0,0,0,0,1,0,1],
+        [1,0,1,1,1,1,0,1,0,1],
+        [1,0,0,0,0,1,0,0,0,1],
+        [1,1,1,1,0,1,1,1,0,1],
+        [1,0,0,0,0,0,0,1,0,1],
+        [1,0,1,1,1,1,0,0,0,1],
+        [1,1,1,1,1,1,1,1,1,1]
     ];
-    mesh = new THREE.Mesh(new THREE.BoxGeometry(10000, 10000, 10000, 7, 7, 7), new THREE.MultiMaterial(materials));
-    mesh.position.y = 1000;
-    mesh.scale.x = -1;
-    scene.add(mesh);
 
-    // Ground
-    geometry = new THREE.PlaneGeometry(1000, 1000, 100, 100);
-    geometry.rotateX(-Math.PI / 2);
-    for (var i = 0, l = geometry.vertices.length; i < l; i++) {
-        var vertex = geometry.vertices[i];
-        vertex.x += Math.random() * 20 - 10;
-        vertex.y += Math.random() * 2;
-        vertex.z += Math.random() * 20 - 10;
-    }
-    for (var i = 0, l = geometry.faces.length; i < l; i++) {
-        var face = geometry.faces[i];
-        face.vertexColors[0] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-        face.vertexColors[1] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
-        face.vertexColors[2] = new THREE.Color().setHSL(Math.random() * 0.3 + 0.5, 0.75, Math.random() * 0.25 + 0.75);
+    var blockSize = 100;
+    var mazeWallGeo = new THREE.BoxGeometry(blockSize, roomHeight, blockSize);
+    var mazeWallMat = new THREE.MeshBasicMaterial({ color: wallColor });
 
-    }
-    material = new THREE.MeshBasicMaterial({vertexColors: THREE.VertexColors, opacity: 0.5, transparent: true});
-    mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-    //var helper = new THREE.GridHelper(500, 10, 0x444444, 0x444444);
-    //helper.position.y = 0.1;
-    //scene.add(helper);
-
-    // Birds
-    mixer = new THREE.AnimationMixer(scene);
-    function addMorph(geometry, speed, duration, x, y, z, fudgeColor) {
-        var material = new THREE.MeshLambertMaterial({
-            color: 0xffaa55,
-            morphTargets: true,
-            vertexColors: THREE.FaceColors
-        });
-        if (fudgeColor) {
-            material.color.offsetHSL(0, Math.random() * 0.5 - 0.25, Math.random() * 0.5 - 0.25);
+    for(var z = 0; z < mazeData.length; z++) {
+        for(var x = 0; x < mazeData[z].length; x++) {
+            if(mazeData[z][x] === 1) {
+                var wall = new THREE.Mesh(mazeWallGeo, mazeWallMat);
+                // Offset calculation to center the maze in the 1000x1000 room
+                wall.position.set(x * blockSize - 450, roomHeight/2 - 5, z * blockSize - 450);
+                scene.add(wall);
+            }
         }
-        var mesh = new THREE.Mesh(geometry, material);
-        mesh.speed = speed;
-        var clip = geometry.animations[0];
-        mixer.clipAction(clip, mesh).setDuration(duration).// to shift the playback out of phase:
-        startAt(-duration * Math.random()).play();
-        mesh.position.set(x, y, z);
-        mesh.rotation.y = Math.PI / 2;
-        mesh.castShadow = true;
-        mesh.receiveShadow = true;
-        scene.add(mesh);
-        morphs.push(mesh);
     }
-    loader.load("models/animated/stork.js", function (geometry) {
-        addMorph(geometry, 350, 1, 500 - Math.random() * 500, 0 + 350, 340, true);
-    });
-    loader.load("models/animated/parrot.js", function (geometry) {
-        addMorph(geometry, 450, 0.5, 500 - Math.random() * 500, 0 + 300, 700, true);
-    });
 
-    // Renderer
-    renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer = new THREE.WebGLRenderer({ antialias: true });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
 
-    // Controls
+    // --- CONTROLS ---
     controls = new THREE.FirstPersonControls(camera, renderer.domElement);
     controls.movementSpeed = 150;
-    controls.lookSpeed = 0.3;
-    controls.lookVertical = true;
-    window.addEventListener('resize', onWindowResize, false);
+    controls.lookSpeed = 0.1; 
+    controls.enabled = false;
+    controls.verticalMin = 0; 
+    controls.verticalMax = Math.PI;
 
-    // Other Players
-    loader.load('./models/skinned/simple/simple.js', function (geometry, materials) {
-        for (var k in materials) {
-            materials[k].skinning = true;
-        }
-        playerFactory = new THREE.SkinnedMesh(geometry, new THREE.MultiMaterial(materials));
-        playerFactory.scale.set(2.5, 2.5, 2.5);
-        playerFactory.position.set(0, 15, 0);
-        playerFactory.skeleton.useVertexTexture = false;
-        //scene.add( skinnedMesh ); // Not add to scene
-        mixer = new THREE.AnimationMixer(playerFactory);
-        mixer.clipAction(playerFactory.geometry.animations[0]).play();
+    document.addEventListener('pointerlockchange', function () {
+        const isLocked = document.pointerLockElement === renderer.domElement;
+        controls.enabled = isLocked;
+        controls.activeLook = false; 
     });
+
+    document.addEventListener('mousemove', function(event) {
+        if (document.pointerLockElement === renderer.domElement && controls.enabled) {
+            controls.lon += (event.movementX || 0) * controls.lookSpeed;
+            controls.lat -= (event.movementY || 0) * controls.lookSpeed;
+            if (controls.lat > 89.99) controls.lat = 89.99;
+            if (controls.lat < -89.99) controls.lat = -89.99;
+        }
+    });
+
+    window.addEventListener('keydown', function(e) {
+        switch(e.code) {
+            case 'Space': if (canJump) velocity.y += 250; canJump = false; break;
+            case 'ControlLeft': isCrouching = true; controls.movementSpeed = 70; break;
+            case 'ShiftLeft': if (!isCrouching) controls.movementSpeed = 300; break;
+        }
+    });
+
+    window.addEventListener('keyup', function(e) {
+        switch(e.code) {
+            case 'ControlLeft': isCrouching = false; controls.movementSpeed = 150; break;
+            case 'ShiftLeft': controls.movementSpeed = 150; break;
+        }
+    });
+
+    window.addEventListener('resize', onWindowResize, false);
 }
 
 function animate() {
     stats.begin();
     requestAnimationFrame(animate);
-    socket.emit('player', [controls.object.position]);
-    // update position of all the bullets
-    for (var i = 0; i < bullets.length; i++) {
-        var bullet = bullets[i].particle;
-        if (bullet) {
-            bullet.position.add(bullets[i].speed);
-            if (( bullet.position.x >= xyzLimit || bullet.position.x <= -xyzLimit ) ||
-                ( bullet.position.y >= 250 || bullet.position.y <= 0 ) ||
-                ( bullet.position.z >= xyzLimit || bullet.position.z <= -xyzLimit )) {
-                // Bullet reached limit?
-                console.log("remove outbounded bullet");
-                scene.remove(bullets[i].particle);
-                bullets.splice(i, 1);
-            }
-            bullet.verticesNeedUpdate = true;
+    var delta = clock.getDelta();
+
+    if (controls.enabled) {
+        velocity.y -= 9.8 * 60.0 * delta; 
+        controls.object.position.y += velocity.y * delta;
+        var targetHeight = isCrouching ? CROUCH_HEIGHT : PLAYER_HEIGHT;
+        if (controls.object.position.y <= targetHeight) {
+            velocity.y = 0;
+            controls.object.position.y = targetHeight;
+            canJump = true;
         }
     }
+
+    socket.emit('player', [controls.object.position]);
+    
+    for (var i = bullets.length - 1; i >= 0; i--) {
+        bullets[i].particle.position.add(bullets[i].speed);
+        if (bullets[i].particle.position.length() > 2000) {
+            scene.remove(bullets[i].particle);
+            bullets.splice(i, 1);
+        }
+    }
+    
     restrictField(controls, xyzLimit);
-    render();
+    render(delta);
     stats.end();
 }
 
@@ -218,74 +192,35 @@ function onWindowResize() {
     controls.handleResize();
 }
 
-function render() {
-    var delta = clock.getDelta();
-    for (var i = 0; i < morphs.length; i++) {
-        morph = morphs[i];
-        morph.position.x += morph.speed * delta;
-        if (morph.position.x > 2000) {
-            morph.position.x = -1000 - Math.random() * 500;
-        }
-    }
+function render(delta) {
     controls.update(delta);
-    if (mixer) mixer.update(delta);
     renderer.render(scene, camera);
 }
 
 function restrictField(controls, restrict) {
-    if (controls.object.position.x > restrict) {
-        controls.object.position.x = restrict;
-    }
-    if (controls.object.position.x < -restrict) {
-        controls.object.position.x = -restrict;
-    }
-    controls.object.position.y = 25; // Height of view = 25
-    if (controls.object.position.z > restrict) {
-        controls.object.position.z = restrict;
-    }
-    if (controls.object.position.z < -restrict) {
-        controls.object.position.z = -restrict;
-    }
+    var pos = controls.object.position;
+    pos.x = Math.max(-restrict, Math.min(restrict, pos.x));
+    pos.z = Math.max(-restrict, Math.min(restrict, pos.z));
 }
+
+renderer.domElement.addEventListener("click", function() {
+    this.requestPointerLock();
+});
 
 function playerHandler(id, position) {
-    console.log("call");
     if (!players[id]) {
-        console.log("add");
-        addPlayer(id, position.x, position.y, position.z);
-    } else {
-        console.log("update");
-        updatePlayer(id, position.x, position.y, position.z);
+        var p = new THREE.Mesh(new THREE.BoxGeometry(10, 20, 10), new THREE.MeshBasicMaterial({color: 0xff0000}));
+        scene.add(p);
+        players[id] = p;
     }
-}
-
-function addPlayer(id, x, y, z) {
-    var playerMesh = playerFactory.clone();
-    playerMesh.position.set(x, y, z);
-    scene.add(playerMesh);
-    mixer = new THREE.AnimationMixer(playerMesh);
-    mixer.clipAction(playerMesh.geometry.animations[0]).play();
-    players[id] = playerMesh;
-
-}
-
-function updatePlayer(id, x, y, z) {
-    var playerMesh = players[id];
-    playerMesh.position.set(x, y, z);
+    players[id].position.set(position.x, position.y, position.z);
 }
 
 function AddBullet(position, speed) {
-    particle = new THREE.Sprite(new THREE.SpriteMaterial({map: bulletMap, color: 0xffffff, fog: true}));
-    particle.position.x = position.x;
-    particle.position.y = position.y;
-    particle.position.z = position.z;
-    particle.scale.x = particle.scale.y = 1.5;
-    bullets.push(new Bullet(particle, speed));
-    scene.add(particle);
-}
-
-function Bullet(particle, speed) {
-    this.particle = particle;
-    this.speed = speed;
-    return this;
+    var m = new THREE.SpriteMaterial({map: bulletMap, color: 0xffffff});
+    var p = new THREE.Sprite(m);
+    p.position.copy(position);
+    p.scale.set(1.5, 1.5, 1.5);
+    bullets.push({particle: p, speed: speed});
+    scene.add(p);
 }
